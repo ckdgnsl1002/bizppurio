3c8a4718d3c52bac44a7404385ae76e468059a88
ckdgnsl1002 <ckdgnsl1002@korea.ac.kr>
2021-10-19 15:27:31 +0900

    친구톡 발송 테스트. (운영버전)

--- biz_msg/practice.py
+++ biz_msg/practice.py
@@ -38,7 +38,7 @@ def send_FT():
         'Content-type' : 'application/json; charset=utf-8',
     }
     
-    body={
+    body_sms={
         "account" : 'lookin4u_dev',
         "refkey" : 'BJUom4GftTE4',
         'type' : 'sms',
@@ -51,11 +51,35 @@ def send_FT():
         }
     }
     
+    body_ft={
+        "account" : 'lookin4u_dev',
+        "refkey" : 'BJUom4GftTE4',
+        'type' : 'ft',
+        'from' : '01095106419',
+        'to' : '01095106419',
+        'content' : {
+            'ft':{
+                'senderkey' : sender_key,
+                "message" : '안녕 원명아! 지금 내 핸드폰번호로 발신하게 설정해보았어. 이 메세지가 너에게 닿길 바래.',
+                'image':{
+                    'img_url' : 'http://street-together.kro.kr/media/blog/images/2021/10/06/53DF48C5-BCC0-4D66-8AC6-5BC147094A57.jpeg',
+                    'img_link' : 'http://street-together.kro.kr/'
+                },
+                'button':{
+                    'name' : '테스트!!',
+                    'type' : "WL",
+                    'url_mobile' : 'http://street-together.kro.kr/',
+                    'url_pc' : 'https://www.naver.com/'
+                }
+            }
+        }
+    }
+    
     url = 'https://api.bizppurio.com/v3/message'
     session = requests.Session()
     # session.verify=False
     # return requests.post(url, headers=headers)
-    return session.post(url, headers=headers, data=json.dumps(body))
+    return session.post(url, headers=headers, data=json.dumps(body_ft))
 
 
 send_complete = send_FT()
